a011dc9e27654c8bb97db78402482cfdc4f0c31c
Jesper Højlund <github@hillgrove.dk>
2024-09-29 17:45:09 +0200

    feat: add MVP TCP Client

--- TCP Client/Program.cs
+++ TCP Client/Program.cs
@@ -1,1 +1,27 @@
 ﻿
+using System.Net.Sockets;
+
+Console.WriteLine("TCP Client");
+
+TcpClient socket = new TcpClient("127.0.0.1", 7);
+NetworkStream ns = socket.GetStream();
+StreamReader reader = new StreamReader(ns);
+StreamWriter writer = new StreamWriter(ns);
+
+bool keepSending = true;
+while (keepSending)
+{
+    string message = Console.ReadLine();
+    writer.WriteLine(message);
+    writer.Flush();
+    
+    string response = reader.ReadLine();
+    Console.WriteLine(response);
+
+    if (message.ToLower() == "stop")
+    {
+        keepSending = false;
+    }
+}
+
+socket.Close();
