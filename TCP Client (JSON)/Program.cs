ea7b1f6905758bc1c95c6564d8039f9b5ffb6c0a
Jesper Højlund <github@hillgrove.dk>
2024-09-29 18:04:59 +0200

    feat: add code from previous assignment with changed port to 1337

---
+++ TCP Client (JSON)/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Net.Sockets;
+
+Console.WriteLine("TCP Client");
+
+TcpClient socket = new TcpClient("127.0.0.1", 1337);
+NetworkStream ns = socket.GetStream();
+StreamReader reader = new StreamReader(ns);
+StreamWriter writer = new StreamWriter(ns);
+
+while (socket.Connected)
+{
+    string message = Console.ReadLine();
+    writer.WriteLine(message);
+    writer.Flush();
+
+    string response = reader.ReadLine();
+    Console.WriteLine(response);
+
+    if (message.ToLower() == "stop")
+    {
+        socket.Close();
+    }
+}
